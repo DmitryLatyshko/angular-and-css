f92db038a0432bc6f42fde2300820f5c1da39dc2
DmitryLatyshko <dmitry.latyshko@outlook.com>
2021-11-02 16:53:07 +0200

    #update

--- src/app/pages/viewencapsulation-emulated/viewencapsulation-emulated-page.component.ts
+++ src/app/pages/viewencapsulation-emulated/viewencapsulation-emulated-page.component.ts
@@ -4,6 +4,11 @@ import { Component } from "@angular/core";
   selector: 'viewencapsulation-emulated-page',
   templateUrl: './viewencapsulation-emulated-page.component.html',
   styleUrls: ['./viewencapsulation-emulated-page.component.scss'],
+  styles: [`.container {
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+  }`]
 })
 export class ViewEncapsulationEmulatedPageComponent {
 
